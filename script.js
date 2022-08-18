ba2c6a828de6345b880193d9e1d1d9eebda10862
Jorin <joserinaldifernandez@gmail.com>
2022-08-18 11:01:16 -0700

    Created a function that generates random selection for computer

---
+++ script.js
@@ -0,0 +1,16 @@
+function computerPlay() {
+    const choices = ["Rock","Paper","Scissors"]
+    let randomSelection = Math.floor(Math.random() * 100) + 1
+
+    if (randomSelection <= 33) {
+        return choices[0]
+    } else if (randomSelection <= 66) {
+        return choices[1]
+    } else {
+        return choices[2]
+    }
+}
+
+let computerChoice = computerPlay();
+
+console.log(computerChoice)
