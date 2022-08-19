f3eb434b32199c7c787ca0779d52d986f22b067b
Jorin <joserinaldifernandez@gmail.com>
2022-08-18 22:02:06 -0700

    Added while loop and game function

--- script.js
+++ script.js
@@ -5,19 +5,23 @@ function computerPlay() {
 }
 
 
-
-let computerChoice = computerPlay();
-let playerChoice = prompt("Please select between Rock, Paper, and Scissors")
+function game () {
+    let rounds = 0;
+    let playerScore = 0;
+    let computerScore= 0;
+    
+while (rounds < 5) {
+    rounds++;
+    computerPlay();
+    playerChoice = prompt("Please select between Rock, Paper, and Scissors")
+    playRound()
+    console.log(playRound(playerCaseSensitive(playerChoice), computerPlay()))
 
 function playerCaseSensitive(playerChoice) {
 return playerChoice[0].toUpperCase() + playerChoice.substring(1).toLowerCase()
 }
 
 function playRound(playerSelection, computerChoice){
-    let playerScore = 0;
-    let computerScore = 0;
-    let rounds = 0;
-
     let tie = "Both sides have the same choice, no points awarded"
     let playerWins = "The player has selected " + playerSelection + " which beats " + computerChoice;
     let playerLoses = "The player has selected " + playerSelection + " which loses to " + computerChoice;
@@ -25,38 +29,26 @@ function playRound(playerSelection, computerChoice){
     if ((playerSelection == "Rock" && computerChoice == "Scissors") ||
         (playerSelection == "Paper" && computerChoice== "Rock") ||
         (playerSelection == "Scissors" && computerChoice == "Paper")) {
-            console.log(playerWins)
+            
             playerScore++
+            return playerWins
         }
 
     else if ((computerChoice== "Rock" && playerSelection== "Scissors") ||
              (computerChoice== "Paper" && playerSelection== "Rock") ||
              (computerChoice == "Scissors" && playerSelection == "Paper")) {
-            console.log(playerLoses)
+         
             computerScore++
+            return playerLoses
         }
     else if (playerChoice == computerChoice) {
-            console.log(tie)
+            return tie
     }
 }
 
-console.log(playRound(playerCaseSensitive(playerChoice),computerChoice))
+}
 
 
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+game()
